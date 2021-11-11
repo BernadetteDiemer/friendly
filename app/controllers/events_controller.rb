b002c68ceeef0c2661963d0cd77a55e3c98584d8
Bernadette Diemer <diemer.lulu@gmail.com>
2021-11-11 21:20:47 +0100

    created the create method, users are now able to create their own events, but the link to event#show isn't functional yet

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,27 @@
 class EventsController < ApplicationController
+  def new
+    @event = Event.new
+    authorize @event
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+    authorize @event
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render :new
+    end
+  end
+
   def index
     @events = policy_scope(Event).order(created_at: :desc)
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :number_of_participants, :address, :date, :languages)
   end
 end
