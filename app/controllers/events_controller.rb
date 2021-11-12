7c1e94df69550d9504f52cb034c19a27321884b6
linda dao <lindadao92@gmail.com>
2021-11-12 15:43:21 +0100

    Add dotenv - Protect my secret data in .env file

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,22 @@
 class EventsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:show, :home, :index]
+  before_action :set_event, only: [:show]
+
   def index
     @events = policy_scope(Event).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :number_of_participants, :address, :date, :languages, photos: [])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
+    authorize @event
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,5 @@ class Event < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one :chatroom, dependent: :destroy
   validates :title, :description, :number_of_participants, :address, :date, presence: true
+  has_one_attached :photo
 end
