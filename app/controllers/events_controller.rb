2c18f74f6039efeb4a8175ef396bf509401d8241
linda dao <lindadao92@gmail.com>
2021-11-17 14:52:55 +0100

    finished bookings index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,8 +24,10 @@ class EventsController < ApplicationController
     }]
     @booking = Booking.new
     @num_accepted_participants = @event.bookings.select { |invite| invite.status == "accepted" }.count
-    @matching_booking = current_user.bookings.select { |b| b.event.id == @event.id }.first
-    @already_booked = !@matching_booking.nil?
+    if current_user != nil
+      @matching_booking = current_user.bookings.select { |b| b.event.id == @event.id }.first
+      @already_booked = !@matching_booking.nil?
+    end
   end
 
   def create
