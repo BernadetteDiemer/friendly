4b909ec4b4e2b2cc23004dd6dac7bab161d5f3c2
Bernadette Diemer <diemer.lulu@gmail.com>
2021-11-18 16:10:59 +0100

    started on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/users/:user_id/events', to: 'users#index', as: 'users_events'
+
   resources :bookings, only: :index
 
   resources :events do
