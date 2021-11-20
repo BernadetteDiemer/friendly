edbb2263ee6afc26a4911159d3e59be2616c8043
Nathalie Faber <nathalie_faber@hotmail.com>
2021-11-20 12:48:24 +0100

    started chatroom

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bookings, only: :index
+  resources :chatrooms, only: :show
+
 
   resources :events do
     resources :bookings, only: [:new, :create, :update, :destroy]
