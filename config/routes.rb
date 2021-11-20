96fc0e6ba35e9108a6de6f1546ac97e465fac11d
Nathalie Faber <nathalie_faber@hotmail.com>
2021-11-20 16:10:38 +0100

    added features chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bookings, only: :index
-  resources :chatrooms, only: :show
-
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
 
   resources :events do
     resources :bookings, only: [:new, :create, :update, :destroy]
