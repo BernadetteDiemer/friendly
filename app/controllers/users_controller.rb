f2cf8059d4a3320703e13419dfba7b28763b31b1
linda dao <lindadao92@gmail.com>
2021-11-22 14:24:32 +0100

    added reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @review = current_user.reviews.last
-    @reviews = current_user.reviews
-    reviewed_user = @review.booking.event.user_id
-    # @reviews = Review.joins(:bookings).where(bookings: { id: @review.booking_id}).joins(:events).where(events: { user_id: reviewed_user})
-    @bookings = current_user.bookings
+    @reviews = Review.joins(booking: :event).where('events.user_id': @user.id).where('bookings.status': 1)
     if @user.birthday
       @age = ((Time.zone.now - @user.birthday.to_time) / 1.year.seconds).floor
     end
