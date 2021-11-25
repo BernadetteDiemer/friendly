66890edbc36a19a8190c0f41f730d6783be6d0a6
Bernadette Diemer <diemer.lulu@gmail.com>
2021-11-25 21:18:15 +0100

    devise stuff to be able to save gender & birthday, set up sign-up-page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:about, :first_name, :last_name, :photo)
+    params.require(:user).permit(:about, :first_name, :last_name, :photo, :birthday, :gender)
   end
 
   def set_user
