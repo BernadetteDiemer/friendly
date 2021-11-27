1f2ae86f6408ecb2e73450822437da488eb48dab
Virna Harri <harrivirna18@gmail.com>
2021-11-27 11:59:50 +0100

    implemented flatpickr for events#new and how date is displayed on events#index & events#show

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,7 +8,7 @@ class Event < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews
   has_one :chatroom, dependent: :destroy
-  validates :title, :description, :number_of_participants, :address, :date, presence: true
+  validates :title, :description, :number_of_participants, :languages, :address, :date, presence: true
   has_one_attached :photo
 
   pg_search_scope :search_by_address_and_date, against: [:address, :date],
