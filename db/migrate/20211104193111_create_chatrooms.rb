8fc12e301e89536df7d0346841b5baa9b6f7c603
Bernadette Diemer <diemer.lulu@gmail.com>
2021-12-02 21:20:36 +0100

    frontend index chatrooms, migrations issues

--- db/migrate/20211104193111_create_chatrooms.rb
+++ db/migrate/20211104193111_create_chatrooms.rb
@@ -2,10 +2,6 @@ class CreateChatrooms < ActiveRecord::Migration[6.0]
   def change
     create_table :chatrooms do |t|
       t.references :event, null: false, foreign_key: true
-<<<<<<< HEAD
-
-=======
->>>>>>> master
       t.timestamps
     end
   end
--- db/migrate/20211130213759_remove_notification_from_bookings.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNotificationFromBookings < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :bookings, :notification, :boolean
-  end
-end
